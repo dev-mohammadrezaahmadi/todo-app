f3b6aeda10b6afa33087f3860c2f9eefaa9c0ab5
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-09-05 22:58:18 +0430

    make a component for items list

---
+++ src/components/ItemsList.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { items } from '../store'
+    function removeTodo(id) {
+		items.update(items => items.filter(item => item.id !== id))
+	}
+</script>
+
+<ul>
+	<h2>undone</h2>
+    {#each $items as item (item.id)}
+	{#if item.done === false}
+		<li>{item.title}</li>
+        <button on:click={() => removeTodo(item.id)}>remove</button>
+        <input bind:checked={item.done} type="checkbox">
+	{/if}
+	{/each}
+
+</ul>
+
+
+<ul>
+	<h2>done</h2>
+    {#each $items as item (item.id)}
+	{#if item.done === true}
+		<li class="done">{item.title}</li>
+        <button on:click={() => removeTodo(item.id)}>remove</button>
+        <input bind:checked={item.done} type="checkbox">
+	{/if}
+	{/each}
+</ul>
+
+<style>
+    .done {
+        text-decoration: line-through;
+    }
+</style>
