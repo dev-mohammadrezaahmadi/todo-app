eec08fd3d107ee028432172aea30ce70c167ab4a
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-09-05 22:58:51 +0430

    use items list component instead of imperative list

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import {v4 as uuidv4} from 'uuid'
 	import { items } from "./store";
+	import ItemsList from './components/ItemsList.svelte'
 	let newTodo = ""
 	function addTodo() {
 		items.update(items => [...items, {
@@ -10,9 +11,8 @@
 		}])
 		newTodo = ''
 	}
-	function removeTodo(id) {
-		items.update(items => items.filter(item => item.id !== id))
-	}
+
+	
 </script>
 
 <svelte:head>
@@ -22,37 +22,11 @@
 <main>
 	<form on:submit|preventDefault={addTodo}  action="">
 	<label for="todo">
-		todo
+		todos
 	</label>
 	<input bind:value={newTodo} type="text" id="todo">
 	<button>add todo</button>
-</form>
-<ul>
-	<h2>undone</h2>
-	{#each $items as item (item.id)}
-	{#if item.done === false}
-		
-	<li>{item.title}</li>
-	<button on:click={() => removeTodo(item.id)}>remove</button>
-	<input bind:checked={item.done} type="checkbox">
-	{/if}
-	{/each}
-</ul>
-<ul>
-	<h2>done</h2>
-	{#each $items as item (item.id)}
-	{#if item.done === true}
-		
-	<li class={item.done && 'done'}>{item.title}</li>
-	<button on:click={() => removeTodo(item.id)}>remove</button>
-	<input bind:checked={item.done} type="checkbox">
-	{/if}
-	{/each}
-</ul>
+	</form>
+	<ItemsList />
 </main>
 
-<style>
-	.done {
-		text-decoration: line-through;
-	}
-</style>
